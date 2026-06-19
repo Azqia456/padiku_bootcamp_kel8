bbfd02eb5ec70aef59684a8a980e2f163af41bf1
thuffyf <19240503@bsi.ac.id>
2026-06-19 10:57:54 +0700

    biar ada frame nya

--- padiku_bootcamp_kel8-main/lib/main.dart
+++ padiku_bootcamp_kel8-main/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_frame/device_frame.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,10 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DeviceFrame(
+        device: Devices.android.samsungGalaxyS20,
+        screen: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
