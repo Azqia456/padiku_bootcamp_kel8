1c92b1391f3a2721855196dfbabe8d968248172e
thuffyf <19240503@bsi.ac.id>
2026-06-19 21:48:17 +0700

    framenya udah ada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_frame/device_frame.dart';
 import 'utils/app_colors.dart';
 import 'utils/routes.dart';
 import 'screens/login_screen.dart';
@@ -12,27 +13,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PADIKU',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: AppColors.riceGreen,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.riceGreen,
-          primary: AppColors.riceGreen,
-          secondary: AppColors.riceYellow,
-        ),
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppColors.backgroundColor,
-        cardColor: AppColors.cardColor,
-        appBarTheme: AppBarTheme(
-          backgroundColor: AppColors.riceGreen,
-          foregroundColor: AppColors.riceWhite,
-          elevation: 0,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: DeviceFrame(
+        device: Devices.android.samsungGalaxyS20,
+        orientation: Orientation.portrait,
+        screen: MaterialApp(
+          title: 'PADIKU',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: AppColors.riceGreen,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: AppColors.riceGreen,
+              primary: AppColors.riceGreen,
+              secondary: AppColors.riceYellow,
+            ),
+            useMaterial3: true,
+            scaffoldBackgroundColor: AppColors.backgroundColor,
+            cardColor: AppColors.cardColor,
+            appBarTheme: AppBarTheme(
+              backgroundColor: AppColors.riceGreen,
+              foregroundColor: AppColors.riceWhite,
+              elevation: 0,
+            ),
+          ),
+          initialRoute: Routes.login,
+          routes: Routes.getRoutes(),
         ),
       ),
-      initialRoute: Routes.login,
-      routes: Routes.getRoutes(),
     );
   }
 }
